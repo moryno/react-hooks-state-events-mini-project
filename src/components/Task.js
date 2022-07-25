428d2319bd96311b773502cba6edf7dc3c2bcc43
Maurice <mauricenganga41@gmail.com>
2022-07-25 09:12:49 +0300

    commit tree 849b6798a7e0b0837d55893a5819511303cc12cb

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Task({task,id, onDelete}) {
+function Task({text, category,id, onDelete}) {
  const handleClick = () => {
    onDelete(id)
  }
   return (
     <div className="task">
-      <div className="label">{task.category}</div>
-      <div className="text">{task.text}</div>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
       <button onClick={handleClick} className="delete">X</button>
     </div>
   );
